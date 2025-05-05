5363018f2df61d957ba851e0cf37e0bd96ca78a6
aadhyanav <126607197+aadhyanav@users.noreply.github.com>
2025-05-04 21:16:50 -0700

    Updates

---
+++ precache-manifest.c0a25099dc4a892c55df9b55a59a0d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338344a8c912460a6aefba8806171ab1",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "24a73305b67a3101b676",
+    "url": "/my-portfolio/static/css/main.0aa88148.chunk.css"
+  },
+  {
+    "revision": "72a285ad32e52f16b27e",
+    "url": "/my-portfolio/static/js/2.efdab03d.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/my-portfolio/static/js/2.efdab03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc5a2d6146a3ecaf9da",
+    "url": "/my-portfolio/static/js/3.e642e0d8.chunk.js"
+  },
+  {
+    "revision": "24a73305b67a3101b676",
+    "url": "/my-portfolio/static/js/main.ea3dd2b9.chunk.js"
+  },
+  {
+    "revision": "e8b523f0acff629d2b93",
+    "url": "/my-portfolio/static/js/runtime-main.ac8b017c.js"
+  }
+]);
